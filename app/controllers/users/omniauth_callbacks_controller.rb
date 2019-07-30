1e40ceee2deba487cd6ca56a82c8dede4b31f80f
kbs4674 <kbs4674@naver.com>
2019-07-30 16:44:26 +0000

    [Step 6] 외부 연동로그인 이벤트 처리를 담당하는 Omniauth_callbacks_controller.rb 내 코드 작성

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,30 +1,27 @@
 # frozen_string_literal: true
 
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  # You should configure your model like this:
-  # devise :omniauthable, omniauth_providers: [:twitter]
+  def self.provides_callback_for(provider)
+    class_eval %Q{
+      def #{provider}
+        @user = User.find_for_oauth(request.env["omniauth.auth"], current_user) # ← 해당 함수 작성 시, app/models/user.rb의 def self.find_for_oauth 함수로 이동 (기존의 User 모델 내에 계정이 있는지 검사, 없으면 새로 계정 생성)
+        
+        if @user.persisted?
+          sign_in_and_redirect @user, event: :authentication
+          set_flash_message(:notice, :success, kind: "#{provider}".capitalize) if is_navigational_format?
+        else
+          session["devise.#{provider}_data"] = request.env["omniauth.auth"]
+          redirect_to new_user_registration_url
+        end
+      end
+    }
+  end
 
-  # You should also create an action method in this controller like this:
-  # def twitter
-  # end
+  [:kakao, :naver, :facebook, :google_oauth2].each do |provider|
+    provides_callback_for provider
+  end
 
-  # More info at:
-  # https://github.com/plataformatec/devise#omniauth
-
-  # GET|POST /resource/auth/twitter
-  # def passthru
-  #   super
-  # end
-
-  # GET|POST /users/auth/twitter/callback
-  # def failure
-  #   super
-  # end
-
-  # protected
-
-  # The path used when OmniAuth fails
-  # def after_omniauth_failure_path_for(scope)
-  #   super(scope)
-  # end
+  def after_sign_in_path_for(resource)
+    root_path
+  end
 end
